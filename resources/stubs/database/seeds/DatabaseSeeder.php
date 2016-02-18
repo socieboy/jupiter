10748bd992ecdbd21b92c5972361ea04fb667533
socieboy <socieboy@gmail.com>
2016-02-18 13:25:01 -0800

    Update workflow for seed database

--- resources/stubs/database/seeds/DatabaseSeeder.php
+++ resources/stubs/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
-
 use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,8 +12,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UserTableSeeder::class);
+        Eloquent::unguard();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        DB::table('permission_role')->truncate();
+        DB::table('role_user')->truncate();
+        DB::table('users')->truncate();
+        DB::table('roles')->truncate();
+        DB::table('permissions')->truncate();
+
+        $this->call(UserTableSeeder::class);
         $this->call(RoleAndPermissionTableSeeder::class);
         $this->call(PermissionTableSeeder::class);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
